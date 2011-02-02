54fb3a5ed11efafbcd0c91027a2e33ff7e025e47
Michael Tremer <michael.tremer@ipfire.org>
2011-02-02 17:58:06 +0100

    build-essentials: Add buildsystem includes to package.

---
+++ pkgs/core/build-essentials/buildsystem.sh
@@ -0,0 +1,8 @@
+
+# Path where the include files of the buildsystem
+# are located.
+export BUILDSYSTEM_ROOT="/usr/lib/buildsystem"
+
+# XXX just for now, we need to set PKGROOT to be compatible
+# with the old makefiles
+export PKGROOT=${BUILDSYSTEM_ROOT}
