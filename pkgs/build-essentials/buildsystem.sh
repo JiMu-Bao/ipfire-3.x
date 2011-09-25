802ea3af55e3167b6574e23b230cdbf0ed2ae0db
Michael Tremer <michael.tremer@ipfire.org>
2011-09-25 11:41:57 +0200

    Change file layout of the makefiles.

--- pkgs/build-essentials/buildsystem.sh
+++
@@ -1,8 +0,0 @@
-
-# Path where the include files of the buildsystem
-# are located.
-export BUILDSYSTEM_ROOT="/usr/lib/buildsystem"
-
-# XXX just for now, we need to set PKGROOT to be compatible
-# with the old makefiles
-export PKGROOT=${BUILDSYSTEM_ROOT}
