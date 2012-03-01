4c928ab7b40674cd7d8badcbf24aa9e3b8da6d4f
Michael Tremer <michael.tremer@ipfire.org>
2012-03-01 19:10:03 +0100

    kernel: Update to Linux 3.2.
    Introduce new way of creating configurations:
    
    From now on, there are no configuration files as they come
    out of "make oldconfig" for every single architecture and kernel
    version.
    
    There is now a system that creates those configurations
    and so it is easier to track changes, see differences between
    architectures and a lot more.
    
    There are configuration files for armv5tel kernels but they
    do not build because of problems with the grsecurity patchset.

---
+++ kernel/scripts/configcommon.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python
+###############################################################################
+# IPFire.org    - An Open Source Firewall Solution                            #
+# Copyright (C) - IPFire Development Team <info@ipfire.org>                   #
+###############################################################################
+
+import re
+import sys
+
+filelist = sys.argv[1:]
+
+lines = []
+options_state   = {}
+options_counter = {}
+
+first_file = True
+for filename in filelist:
+	f = open(filename)
+
+	for line in f.readlines():
+		# Strip newline.
+		line = line.rstrip()
+
+		if line.startswith("# Automatically generated file;"):
+			continue
+
+		if line.endswith("Kernel Configuration"):
+			continue
+
+		option = value = None
+
+		m = re.match("^# (.*) is not set$", line)
+		if m:
+			option = m.group(1)
+			value  = "n"
+
+		m = re.match("^(.*)=(.*)$", line)
+		if m:
+			option = m.group(1)
+			value  = m.group(2)
+
+		if option:
+			option_value = "%s=%s" % (option, value or "")
+
+			try:
+				options_counter[option_value] += 1
+				continue
+
+			except KeyError:
+				options_counter[option_value] = 1
+
+		if first_file:
+			lines.append(line)
+
+	f.close()
+	first_file = False
+
+for line in lines:
+	m = re.match("^# (.*) is not set$", line)
+	if m:
+		if options_counter.get("%s=n" % m.group(1), 0) == len(filelist):
+			print "# %s is not set" % m.group(1)
+
+		continue
+
+	m = re.match("^(.*)=(.*)$", line)
+	if m:
+		if options_counter.get(m.group(0), 0) == len(filelist):
+			print m.group(0)
+
+		continue
+
+	print line
